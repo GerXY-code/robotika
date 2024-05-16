fb5565810213de21b29931f4cda00e21b5c24a41
Mark Marada <maradamark99@gmail.com>
2024-05-16 16:23:56 +0200

    Fix sweeping functionality

--- src/Locator.cpp
+++ src/Locator.cpp
@@ -1,16 +1,13 @@
 #include "Locator.h"
-#include "Arduino.h"
 
-void Locator::sweep(uint16_t servoStartingPos, uint16_t boundaryToLeft, uint16_t boundaryToRight) {
-  uint16_t i = servoStartingPos;
-  while (i < boundaryToRight) {
-    m_servo->write(i);
-    i++;
+void Locator::sweep(uint16_t servoStartingPos, uint16_t boundary) {
+  int16_t pos;
+  for (pos = servoStartingPos; pos <= boundary; pos += 1) {
+    m_servo->write(pos);
     delay(15);
   }
-  while (i > 0) {
-    m_servo->write(i);
-    i--;
-    delay(15);
+  for (pos = boundary; pos >= 0; pos -= 1) {
+    m_servo->write(pos);              
+    delay(15);                     
   }
 }
