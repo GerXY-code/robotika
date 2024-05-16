f3f86c8ce1d0b2d2b02bd5f301a7ff41f7a7acda
Mark Marada <maradamark99@gmail.com>
2024-05-16 16:23:27 +0200

    Renamed Gyro class to IGyro (interface) and implemented functionality

--- src/Locator.h
+++ src/Locator.h
@@ -1,16 +1,17 @@
 #ifndef LOCATOR_H
 #define LOCATOR_H
 #include <Servo.h>
-#include "Gyro.h"
+#include "Arduino.h"
+#include "IGyro.h"
 #include "IDistanceSensor.h"
 
 class Locator {
   private:
     IDistanceSensor *m_distSensor;
-    Gyro *m_gyro;
+    IGyro *m_gyro;
     Servo *m_servo;
   public: 
-    Locator(IDistanceSensor *distSensor, Gyro *gyro, Servo *servo) {
+    Locator(IDistanceSensor *distSensor, IGyro *gyro, Servo *servo) {
       m_distSensor = distSensor;
       m_gyro = gyro;
       m_servo = servo;
@@ -20,7 +21,7 @@ class Locator {
       delete m_gyro;
       delete m_servo;
     }
-    void sweep(uint16_t servo_starting_pos, uint16_t boundaryToLeft, uint16_t boundaryToRight);
+    void sweep(uint16_t servoStartingPos, uint16_t boundary);
 };
 
 #endif
